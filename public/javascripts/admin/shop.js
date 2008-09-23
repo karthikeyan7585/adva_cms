cd877270cc40c79e6c257ca697a5aa0c325cf23b
root <root@aspire038.aspiresys.com>
2008-09-23 23:02:43 +0530

    updated

--- public/javascripts/admin/shop.js
+++
@@ -1,6 +0,0 @@
-function togglePaymentMethod(checked, paymentMethodDivId){
-	if(checked == true)
-		document.getElementById(paymentMethodDivId).style.display = '';
-	else
-		document.getElementById(paymentMethodDivId).style.display = 'none';
-}
